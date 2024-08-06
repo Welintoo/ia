f21f1f97d7678cf7b0bee731d08f3f74901e343b
Welintoo <165098324+Welintoo@users.noreply.github.com>
2024-08-06 20:10:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,29 +7,37 @@ const pergunta = [
     {
         enunciado: "você esta entediado(a) e deside mecher no PC  ",
         alternativas: [
-            "entrar em um bate papo online",
-            "jogar um jogo online"
+            {
+            texto: "entrar em um bate papo online.",
+           afirmação: "jogar um jogo online."
+            },
         ]
     },
     {
         enunciado: "você encontra uma outra pessoa irritado discutindo com seu colega",
         alternativas: [
-            "entrar na discussão",
-            "tentar entender a discussão"
+            {
+           texto: "entrar na discussão",
+            afirmação: "tentar entender a discussão"
+            },
         ]
     },
     {
         enunciado: "eles descutem sobre qual é o certo entre bolacha e biscoito, eles le perguntão qual é o certo",
         alternativas: [
-            "você responde bislacha",
-            "você  responde bolabiz"
+            {
+           texto: "você responde bislacha",
+           afirmação "você  responde bolabiz"
+            },
         ]
     },
     {
         enunciado: "eles falam que isso não existe ",
         alternativas: [
-            "você fica quieto e vai dormir",
-            "você joga mais outro jogo ate de noite"
+            {
+           texto: "você fica quieto e vai dormir",
+            afirmação: "você joga mais outro jogo ate de noite"
+            },
         ]
     },
     {
